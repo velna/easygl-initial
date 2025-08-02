e788c7398857959c866162cc6bde39de4e20f2b5
velna <velna_007@163.com>
2025-08-02 21:38:48 +0800

    LearnOpenGl Exercises 3

---
+++ learnopengl/src/main/java/com/vanix/easygl/learnopengl/c1_gettingstarted/C25HelloTriangleExercises3.java
@@ -0,0 +1,92 @@
+package com.vanix.easygl.learnopengl.c1_gettingstarted;
+
+import com.vanix.easygl.core.graphics.*;
+import com.vanix.easygl.core.window.Keyboard;
+import com.vanix.easygl.core.window.Window;
+import com.vanix.easygl.core.window.WindowHints;
+import com.vanix.easygl.glfw.GlWindow;
+import com.vanix.easygl.opengl.GlGraphics;
+
+public class C25HelloTriangleExercises3 {
+    public static void main(String[] args) {
+        WindowHints.ContextVersionMajor.set(3);
+        WindowHints.ContextVersionMinor.set(3);
+        WindowHints.OpenGlProfile.Core.set();
+        try (var window = Window.of(800, 600, "LearnOpenGL");
+             var graphics = new GlGraphics();
+             var vertex = Shader.vertex("v1");
+             var fragmentOrange = Shader.fragment("f-orange");
+             var fragmentYellow = Shader.fragment("f-yellow");
+             var programOrange = Program.of("p-orange");
+             var programYellow = Program.of("p-yellow");
+             var vaos = VertexArray.of(2);
+             var vbos = Buffer.of(2, Buffer.Type.Array, DataType.Float)) {
+            window.bind().inputCtlr().keyboard().onKey(Keyboard.KEY_ESCAPE).subscribe(C25HelloTriangleExercises3::processInput);
+            graphics.viewPort(0, 0, window.frameBufferWidth(), window.frameBufferHeight());
+
+            vertex.source("""
+                            #version 330 core
+                            layout (location = 0) in vec3 aPos;
+                            void main() {
+                                gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+                            }
+                            """)
+                    .compile();
+
+            programOrange.attach(vertex)
+                    .attach(fragmentOrange.source("""
+                                    #version 330 core
+                                    out vec4 FragColor;
+                                    void main(){
+                                        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                                    }
+                                    """)
+                            .compile())
+                    .link();
+            programYellow.attach(vertex)
+                    .attach(fragmentYellow.source("""
+                                    #version 330 core
+                                    out vec4 FragColor;
+                                    void main(){
+                                        FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+                                    }
+                                    """)
+                            .compile())
+                    .link();
+
+            vaos.getFirst().bind().attributes(vbos.getFirst().bind()
+                    .realloc(Buffer.DataUsage.STATIC_DRAW, new float[]{
+                            // first triangle
+                            -0.9f, -0.45f, 0.0f,  // left
+                            -0.0f, -0.45f, 0.0f,  // right
+                            -0.45f, 0.45f, 0.0f
+                    }), 3);
+
+            vaos.getLast().bind().attributes(vbos.getLast().bind()
+                    .realloc(Buffer.DataUsage.STATIC_DRAW, new float[]{
+                            // second triangle
+                            0.0f, -0.45f, 0.0f,  // left
+                            0.9f, -0.45f, 0.0f,  // right
+                            0.45f, 0.45f, 0.0f   // top
+                    }), 3);
+
+            while (!window.shouldClose()) {
+                graphics.clearColor(0.2f, 0.3f, 0.3f, 1.0f)
+                        .clear(Graphics.BufferMask.Color);
+
+                programOrange.bind();
+                vaos.getFirst().drawArray(DrawMode.Triangles, vbos.getFirst());
+                programYellow.bind();
+                vaos.getLast().drawArray(DrawMode.Triangles, vbos.getLast());
+
+                window.swapBuffers().pollEvents();
+            }
+        } finally {
+            GlWindow.systemTerminate();
+        }
+    }
+
+    private static void processInput(Keyboard keyboard, int key, int scancode, int action, int modifiers) {
+        keyboard.getWindow().shouldClose(true);
+    }
+}
