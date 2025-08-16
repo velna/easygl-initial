9d5772d04190454fcd614f4877c4d0dd532b438b
velna <velna_007@163.com>
2025-08-17 01:03:15 +0800

    add some graphics functions

--- easygl-core/src/main/java/com/vanix/easygl/core/graphics/Graphics.java
+++ easygl-core/src/main/java/com/vanix/easygl/core/graphics/Graphics.java
@@ -2,6 +2,7 @@ package com.vanix.easygl.core.graphics;
 
 import com.vanix.easygl.core.Closeable;
 import com.vanix.easygl.core.window.Window;
+import org.joml.Vector2f;
 
 import java.util.ServiceLoader;
 
@@ -22,6 +23,24 @@ public interface Graphics extends Closeable {
     Graphics flush();
 
     Graphics releaseShaderCompiler();
+
+    Vector2f getMultiSample(int index);
+
+    Graphics minSampleShading(float value);
+
+    Graphics pointSize(float size);
+
+    Graphics pointFadeThresholdSize(float size);
+
+    Graphics pointSpriteCoordOrigin(SpriteCoordOrigin spriteCoordOrigin);
+
+    Graphics lineWidth(float width);
+
+    Graphics frontFaceClockwise();
+
+    Graphics frontFaceCounterclockwise();
+
+    Graphics polygonOffset(float factor, float units);
 
     static Graphics of() {
         return ServiceLoader.load(Graphics.class).findFirst().orElseThrow();
--- easygl-opengl/src/main/java/com/vanix/easygl/opengl/GlGraphics.java
+++ easygl-opengl/src/main/java/com/vanix/easygl/opengl/GlGraphics.java
@@ -1,6 +1,7 @@
 package com.vanix.easygl.opengl;
 
 import com.vanix.easygl.core.graphics.*;
+import org.joml.Vector2f;
 import org.lwjgl.opengl.GL;
 
 public class GlGraphics implements Graphics {
@@ -55,6 +56,67 @@ public class GlGraphics implements Graphics {
     @Override
     public Graphics releaseShaderCompiler() {
         GLX.glReleaseShaderCompiler();
+        return this;
+    }
+
+    @Override
+    public Vector2f getMultiSample(int index) {
+        float[] data = new float[2];
+        GLX.glGetMultisamplefv(GLX.GL_SAMPLE_POSITION, index, data);
+        GLX.checkError();
+        return new Vector2f(data);
+    }
+
+    @Override
+    public Graphics minSampleShading(float value) {
+        GLX.glMinSampleShading(value);
+        return this;
+    }
+
+    @Override
+    public Graphics pointSize(float size) {
+        GLX.glPointSize(size);
+        GLX.checkError();
+        return this;
+    }
+
+    @Override
+    public Graphics pointFadeThresholdSize(float size) {
+        GLX.glPointParameterf(GLX.GL_POINT_FADE_THRESHOLD_SIZE, size);
+        GLX.checkError();
+        return this;
+    }
+
+    @Override
+    public Graphics pointSpriteCoordOrigin(SpriteCoordOrigin spriteCoordOrigin) {
+        GLX.glPointParameteri(GLX.GL_POINT_SPRITE_COORD_ORIGIN, spriteCoordOrigin.value());
+        GLX.checkError();
+        return this;
+    }
+
+    @Override
+    public Graphics lineWidth(float width) {
+        GLX.glLineWidth(width);
+        GLX.checkError();
+        return this;
+    }
+
+    @Override
+    public Graphics frontFaceClockwise() {
+        GLX.glFrontFace(GLX.GL_CW);
+        return this;
+    }
+
+    @Override
+    public Graphics frontFaceCounterclockwise() {
+        GLX.glFrontFace(GLX.GL_CCW);
+        return this;
+    }
+
+    @Override
+    public Graphics polygonOffset(float factor, float units) {
+        GLX.glPolygonOffset(factor, units);
+        GLX.checkError();
         return this;
     }
 
