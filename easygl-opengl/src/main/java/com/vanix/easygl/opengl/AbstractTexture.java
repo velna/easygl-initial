fe68d37c7182dfdeccc39cc0c8b36f7b6e7019b8
velna <velna_007@163.com>
2025-09-08 23:44:52 +0800

    add texture level parameters

--- easygl-opengl/src/main/java/com/vanix/easygl/opengl/AbstractTexture.java
+++ easygl-opengl/src/main/java/com/vanix/easygl/opengl/AbstractTexture.java
@@ -1,6 +1,8 @@
 package com.vanix.easygl.opengl;
 
 import com.vanix.easygl.core.AbstractBindable;
+import com.vanix.easygl.core.graphics.DataType;
+import com.vanix.easygl.core.graphics.InternalPixelFormat;
 import com.vanix.easygl.core.graphics.Texture;
 
 import java.util.function.IntConsumer;
@@ -14,6 +16,96 @@ public abstract class AbstractTexture<T extends Texture<T>> extends AbstractBind
 
     public int targetValue(boolean proxy) {
         return target.value(proxy);
+    }
+
+    @Override
+    public int alphaSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_ALPHA_SIZE);
+    }
+
+    @Override
+    public DataType alphaType(int level) {
+        return Cache.DataType.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_ALPHA_TYPE));
+    }
+
+    @Override
+    public int blueSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_BLUE_SIZE);
+    }
+
+    @Override
+    public DataType blueType(int level) {
+        return Cache.DataType.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_BLUE_TYPE));
+    }
+
+    @Override
+    public int bufferOffset(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_BUFFER_OFFSET);
+    }
+
+    @Override
+    public int bufferSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_BUFFER_SIZE);
+    }
+
+    @Override
+    public int compressedImageSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_COMPRESSED_IMAGE_SIZE);
+    }
+
+    @Override
+    public int depth(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_DEPTH);
+    }
+
+    @Override
+    public int depthSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_DEPTH_SIZE);
+    }
+
+    @Override
+    public DataType depthType(int level) {
+        return Cache.DataType.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_DEPTH_TYPE));
+    }
+
+    @Override
+    public int greenSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_GREEN_SIZE);
+    }
+
+    @Override
+    public DataType greenType(int level) {
+        return Cache.DataType.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_GREEN_TYPE));
+    }
+
+    @Override
+    public int height(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_HEIGHT);
+    }
+
+    @Override
+    public InternalPixelFormat internalFormat(int level) {
+        return Cache.InternalPixelFormatCache.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_INTERNAL_FORMAT));
+    }
+
+    @Override
+    public boolean isCompressed(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_COMPRESSED) == GLX.GL_TRUE;
+    }
+
+    @Override
+    public int redSize(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_RED_SIZE);
+    }
+
+    @Override
+    public DataType redType(int level) {
+        return Cache.DataType.get(GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_RED_TYPE));
+    }
+
+    @Override
+    public int width(int level) {
+        return GLX.glGetTexLevelParameteri(target.value(), level, GLX.GL_TEXTURE_WIDTH);
     }
 
     @Override
